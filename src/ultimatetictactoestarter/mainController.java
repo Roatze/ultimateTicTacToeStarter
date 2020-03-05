a20228e434c69284ad965ae13b458dee15ee8050
Roatze_ <RoatzeTFM@Gmail.com>
2020-03-05 22:28:11 +0100

    Jeg Giver Op
    Jeg Giver Op

--- src/ultimatetictactoestarter/mainController.java
+++ src/ultimatetictactoestarter/mainController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 /**
  *
@@ -18,16 +19,9 @@ import javafx.scene.control.Label;
  */
 public class mainController implements Initializable
 {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event)
-    {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private GridPane macroBoard;
     
     @Override
     public void initialize(URL url, ResourceBundle rb)
